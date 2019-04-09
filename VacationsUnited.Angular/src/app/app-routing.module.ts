a9a13c71f7d20d340b6ea8a6fb9e352b995fd8bd
Max Griffin <maxwell.griffin@outlook.com>
2019-04-09 15:32:04 -0400

    angular update

--- VacationsUnited.Angular/src/app/app-routing.module.ts
+++ VacationsUnited.Angular/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Import every component that you are going to make a route for 
+import { HomeComponent }      from './home/home.component';
+
+//Add routes here
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
