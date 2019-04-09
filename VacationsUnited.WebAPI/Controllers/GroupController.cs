e65017452f56ab7b78676f6cc07cc3b415e20781
Joshua Riley <joshriley4@yahoo.com>
2019-04-09 11:50:51 -0400

    first

---
+++ VacationsUnited.WebAPI/Controllers/GroupController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VacationsUnited.Services;
+
+namespace VacationsUnited.WebAPI.Controllers
+{
+    [Authorize]
+    public class GroupController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            GroupService groupService = CreateGroupService();
+            var groups = groupService.GetGroups();
+            return Ok(groups);
+        }
+
+        private GroupService CreateGroupService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var groupService = new GroupService(userId);
+            return groupService;
+        }
+        
+    }
+}
