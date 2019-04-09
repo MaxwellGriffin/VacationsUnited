64b1cfbdf2f42880ea3c1d5862967396d4cf2d1f
Max Griffin <maxwell.griffin@outlook.com>
2019-04-09 10:24:52 -0400

    working on destinationservice

---
+++ VacationsUnited.Services/DestinationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsUnited.Data;
+
+namespace VacationsUnited.Services
+{
+    public class DestinationService
+    {
+        private readonly Guid _userId;
+
+        public DestinationService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(DestinationCreate model)
+        {
+            var entity =
+                new Destination()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
