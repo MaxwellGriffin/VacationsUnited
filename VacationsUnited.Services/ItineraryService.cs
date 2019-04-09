f8de471c9bc704dc2c85c80b0bb21f857cbbc361
Debbie <debbieforbeck@gmail.com>
2019-04-09 11:34:02 -0400

    Itineary Update

---
+++ VacationsUnited.Services/ItineraryService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationsUnited.Services
+{
+    class ItineraryService
+    {
+        private readonly Guid _userId;
+
+        public ItineraryService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateItinerary (ItineraryCreate model)
+        {
+            var entity = new ItineraryService()
+            {
+                ItineraryID = model.ItineraryID,
+                OwnerID = model.OwnerID,
+                GroupID = model.GroupID,
+                DestinationId = model.DestinationId,
+                Date = model.Date,
+                Name = model.Name
+            };
+
+
+
+        }
+
+    }
+}
---
+++ VacationsUnited.WebAPI/Controllers/ItineraryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VacationsUnited.WebAPI.Controllers
+{
+    [Authorize]
+    public class ItineraryController : Controller
+    {
+        // GET: Itinerary
+
+            public IHttpActionResult Get()
+        {
+            ItineraryService itineraryService = CreateItineraryService();
+            var itineraries = itineraryService.GetItineraries();
+            return (itineraries);
+
+        }
+
+        private ItineraryService CreateItineraryService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var itineraryService = new ItineraryService(userId);
+            return itineraryService;
+        }
+    }
+}
