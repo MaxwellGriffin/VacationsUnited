944794bd0e6ab22560bff6e8446d3a77323b6032
Max Griffin <maxwell.griffin@outlook.com>
2019-04-15 14:38:41 -0400

    fixed destinationlistitem

--- VacationsUnited.Models/Destination/DestinationListItem.cs
+++ VacationsUnited.Models/Destination/DestinationListItem.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static VacationsUnited.Data.Enums;
 
 namespace VacationsUnited.Models
 {
     public class DestinationListItem
     {
         public int DestinationID { get; set; }
+        public Regions Region { get; set; }
+        public TripTypes TripType { get; set; }
+        public decimal Price { get; set; }
         public string Name { get; set; }
+        public int MinGuests { get; set; }
+        public int MaxGuests { get; set; }
         public string Location { get; set; }
     }
 }
